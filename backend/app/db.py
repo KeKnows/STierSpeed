984b1195020c886dde6d5b486274c27ab94622d4
Keonne Gladden (KeKnows) <gladdenk.24@william.jewell.edu>
2025-12-05 11:58:22 -0600

    Set up SQLAlchemy engine and session for database

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,1 +1,10 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+Base = declarative_base()
+metadata = MetaData()
 
